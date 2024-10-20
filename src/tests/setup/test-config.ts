ad3738a6bac5afe7028bfd8e1b3632fee4407b64
Anthony Cyrille <ancyr@pm.me>
2024-10-20 08:55:13 +0200

    task: test config

--- src/tests/setup/test-config.ts
+++ src/tests/setup/test-config.ts
@@ -1,9 +1,9 @@
 export const testConfig = () => ({
   DATABASE_URL: `postgresql://user:azerty123@localhost:${process.env.COMPOSE_DB_PORT}/rentmyhouse`,
+  REDIS_CONNECTION_URL: `redis://default@localhost:${process.env.COMPOSE_REDIS_PORT}`,
   ENVIRONMENT: 'test',
   SMTP_HOST: 'mailer',
   SMTP_PORT: process.env.COMPOSE_MAILER_SMTP_PORT,
   SMTP_USER: '',
   SMTP_PASSWORD: '',
-  REDIS_CONNECTION_URL: `redis://default@localhost:${process.env.COMPOSE_REDIS_PORT}`,
 });
